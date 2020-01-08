41dd869b401ba7c62e33680bab3c1a2d117a4e15
ARAB <ahmedmohamed23345@gmail.com>
2020-01-08 13:12:11 +0200

    Initial commit

---
+++ app/src/main/java/com/endpoint/gably/models/PhotosModel.java
@@ -0,0 +1,11 @@
+package com.endpoint.gably.models;
+
+import java.io.Serializable;
+
+public class PhotosModel implements Serializable {
+    private String photo_reference;
+
+    public String getPhoto_reference() {
+        return photo_reference;
+    }
+}
